11fb4331382e35d4eb444326e7630e86d5c8e683
Unknown <rudy512@hotmail.fr>
2018-06-19 15:37:44 +0200

    re comit

--- src/test/java/factory/test/FactoryBootApplicationTests.java
+++ src/test/java/factory/test/FactoryBootApplicationTests.java
@@ -23,6 +23,8 @@ import factory.model.LvlStagiaire;
 import factory.model.Matiere;
 import factory.model.Module;
 import factory.model.Ordinateur;
+import factory.model.Stagiaire;
+import factory.model.Technicien;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
